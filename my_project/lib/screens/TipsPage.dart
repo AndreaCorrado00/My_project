e35eedd05d07af4c9c6070c6550542fab4bb5959
AndreaCorrado00 <corradoandrea00@gmail.com>
2023-05-16 15:54:15 +0200

    get step data

--- my_project/lib/screens/TipsPage.dart
+++ my_project/lib/screens/TipsPage.dart
@@ -1,13 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:my_project/screens/HomePage.dart';
 import 'package:my_project/screens/StatisticsPage.dart';
 import 'package:http/http.dart' as http;
 import 'package:my_project/utils/impact.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:my_project/utils/constants.dart';
+import 'package:my_project/Models/Steps.dart';
 
 class TipsPage extends StatefulWidget {
   const TipsPage({super.key});
@@ -36,15 +39,13 @@ class TipsPageState extends State<TipsPage> {
         
         child: Column(
         children:[ TextButton (
-          
           onPressed: () async{
             final resoult = await _pingImpact();
             if (resoult == true){print('Request Succesfull'); }
-            
           },
           child: Text('ping Impact'), style: Constants.TextButtonStyle_Alert),
+
           TextButton (
-          
           onPressed: () async{
             final resoult = await _getAndStoreTokens();
             if (resoult == 200){print('Stored');}
@@ -59,7 +60,22 @@ class TipsPageState extends State<TipsPage> {
             if (resoult == 200){print('refreshed');}
             else {print('No way ');} 
           },
-          child: Text('refresh tokens'), style: Constants.TextButtonStyle_Alert),],),),
+          child: Text('refresh tokens'), style: Constants.TextButtonStyle_Alert),
+          
+          ElevatedButton(
+                onPressed: () async {
+                  final result = await _getStep();
+                  print(result);
+                  final message =
+                      result == null ? 'Request failed' : 'Request successful';
+                  ScaffoldMessenger.of(context)
+                    ..removeCurrentSnackBar()
+                    ..showSnackBar(SnackBar(content: Text(message)));
+                },
+                child: Text('Get the data')),
+
+          ],
+          ),),
 
 
 
@@ -181,5 +197,40 @@ class TipsPageState extends State<TipsPage> {
     //Just return the status code
     return response.statusCode;
     
-  } //_refreshTokens
+  }
+  
+  Future _getStep()async{
+    final sp=await SharedPreferences.getInstance();
+    var access=sp.getString('access');
+    if (access == null){
+      return false;
+    }
+    else{
+      if(JwtDecoder.isExpired(access!)){
+        await _refreshTokens();
+        access = sp.getString('access');
+      }
+      final day='2023-05-15';
+      final url=Impact.baseUrl+Impact.stepEndpoint + 'patients/Jpefaq6m58'+'/day/$day/';
+
+      final headers = {HttpHeaders.authorizationHeader: 'Bearer $access'}; //fixed costruction!
+
+      final response = await http.get(Uri.parse(url), headers: headers);
+
+      // Now we have made the request ad, probably, the response. But how to get data?
+
+      //if OK parse the response, otherwise return null
+    if (response.statusCode == 200) {
+      final decodedResponse = jsonDecode(response.body);
+      List result = [];
+      for (var i = 0; i < decodedResponse['data']['data'].length; i++) {
+        result.add(Steps.fromJson(decodedResponse['data']['date'], decodedResponse['data']['data'][i]));
+      }//for
+    } //if
+    else{
+      void result = null;
+    }//else
+    }
+
+  }//getStep
 }
--- my_project/lib/utils/impact.dart
+++ my_project/lib/utils/impact.dart
@@ -4,7 +4,7 @@ class Impact {
   static String pingEndpoint = 'gate/v1/ping/';
   static String tokenEndpoint = 'gate/v1/token/';
   static String refreshEndpoint = 'gate/v1/refresh/';
-  
+  static String stepEndpoint = 'data/b1/steps';
   static String username = 'iIWxjdSTr9';
   static String password = '12345678!';
   //static final 
